a8236213e2d2f154a145797acfb88dfa9e21f354
nfw64 <chopopzml@gmail.com>
2024-03-22 13:47:28 +0000

    treewide : compiler adjustment

--- compile.sh
+++ compile.sh
@@ -5,10 +5,19 @@ BUILDDATE=$(date +%H%M)
 
 # Start
 echo "Build started"
-# Check if gitpod and install dependencies
-if [ -d /workspace ] ; then
-sudo apt update -y && sudo apt upgrade -y && sudo apt install nano bc bison ca-certificates curl flex gcc git libc6-dev libssl-dev openssl python-is-python3 ssh wget zip zstd sudo make clang gcc-arm-linux-gnueabi software-properties-common build-essential libarchive-tools gcc-aarch64-linux-gnu -y && sudo apt install build-essential -y && sudo apt install libssl-dev libffi-dev libncurses5-dev zlib1g zlib1g-dev libreadline-dev libbz2-dev libsqlite3-dev make gcc -y && sudo apt install pigz -y && sudo apt install python2 -y && sudo apt install python3 -y && sudo apt install cpio -y && sudo apt install lld -y
-fi
+
+check_deps () {
+DEPS=$(echo {nano bc bison gcc clang make})
+for i in $DEPS ; do
+    dpkg-query -W -f='${Package}\n' | grep ^$i$ > /dev/null
+    if [ $? != 0 ] ; then
+        echo "Installing deps ..."
+        sudo apt update -y && sudo apt upgrade -y && sudo apt install nano bc bison ca-certificates curl flex gcc git libc6-dev libssl-dev openssl python-is-python3 ssh wget zip zstd sudo make clang gcc-arm-linux-gnueabi software-properties-common build-essential libarchive-tools gcc-aarch64-linux-gnu -y && sudo apt install build-essential -y && sudo apt install libssl-dev libffi-dev libncurses5-dev zlib1g zlib1g-dev libreadline-dev libbz2-dev libsqlite3-dev make gcc -y && sudo apt install pigz -y && sudo apt install python2 -y && sudo apt install python3 -y && sudo apt install cpio -y && sudo apt install lld -y
+    fi
+done  
+}
+
+check_deps
 
 # Set variable
 KSU_GIT_VERSION=$(cd KernelSU && git rev-list --count HEAD)
